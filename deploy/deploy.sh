98182baa354a0747b6765179b2965f3c4a86eb90
YairTawil <yairtawil@gmail.com>
2019-01-05 22:38:02 +0200

    deployment

---
+++ deploy/deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+target=$1
+version=$2
+cluster=$3
+service=$4
+region=$5
+
+echo "start deploying version $version on target $target"
+
+# Login to the Docker registry
+eval $(aws ecr get-login --no-include-email --region $region)
+
+# Build
+docker build -t "$target:$version" .
+
+# Tag
+docker tag "$target:$version" "$target:latest"
+
+# Push the new image
+docker push "$target:latest"
+docker push "$target:$version"
+
+echo "deployment succeeded";
+
+node scripts/kill-tasks.js $cluster $service $region
