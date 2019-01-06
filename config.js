b4450a2decb7f856fc07d7114a8b80fd3cf555bf
YairTawil <yairtawil@gmail.com>
2019-01-06 13:16:56 +0200

    update config url's

--- config.js
+++ config.js
@@ -13,11 +13,11 @@ const config = {
   domain: BASE_DOMAIN || 'http://ansyn.webiks.com',
   proxyPorts: {
     'ansyn.webiks.com': ANSYN_PROD_PORT || 83,
-    'ansyn.dev.webiks.com': ANSYN_DEV_PORT || 81,
-    'ansyn.drone-geo-referencer-client.webiks.com': ANSYN_DRONE_GEO_REFERENCER_CLIENT_PORT || 84,
-    'ansyn.dev.storage-service.webiks.com': ANSYN_STORAGE_SERVICE_PROD_PORT || 8080,
-    'ansyn.storage-service.webiks.com': ANSYN_STORAGE_SERVICE_DEV_PORT || 8081,
-    'ansyn.mongo.webiks.com': ANSYN_MONGO_PORT || 85
+    'dev.ansyn.webiks.com': ANSYN_DEV_PORT || 81,
+    'drone-geo-referencer-client.ansyn.webiks.com': ANSYN_DRONE_GEO_REFERENCER_CLIENT_PORT || 84,
+    'storage-service.dev.ansyn.webiks.com': ANSYN_STORAGE_SERVICE_PROD_PORT || 8080,
+    'storage-service.ansyn.webiks.com': ANSYN_STORAGE_SERVICE_DEV_PORT || 8081,
+    'mongo.ansyn.webiks.com': ANSYN_MONGO_PORT || 85
   }
 }
 
